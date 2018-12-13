06bf0292c7ec5021ef730827e47f89add64182f3
destinymingyun <784172167@qq.com>
2018-12-13 09:42:45 +0800

    导入了codeUI

---
+++ management/src/main/java/com/system/management/DocText.java
@@ -0,0 +1,20 @@
+package com.system.management;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * tsdfkjsadhfasfas
+ */
+public class DocText {
+    private String name;
+
+    /**
+     * 这个函数是做好事情的
+     * @param kyy
+     * @return
+     * @throws Exception
+     */
+    public String add(String kyy) throws Exception{
+        return null;
+    }
+}
