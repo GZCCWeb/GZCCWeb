539490fe4dfe9ffe591a34e5ced83c544c8f9527
Richard <1265672126@qq.com>
2018-12-24 16:42:14 +0800

    tenant

--- management/src/main/java/com/system/management/dao/CommentMapper.java
+++ management/src/main/java/com/system/management/dao/CommentMapper.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 @Mapper
 public interface CommentMapper {
-    @Select("SELECT * FROM comment order by id DESC limit 10")
+    //@Select("SELECT * FROM comment order by id DESC limit 10")
     List<Comment> getComment();
 
-    @Insert("INSERT INTO comment (username,text)VALUES(#{username},#{text})")
+    //@Insert("INSERT INTO comment (username,text)VALUES(#{username},#{text})")
     int saveComment(@Param("username") String username,@Param("text") String text);
 }
