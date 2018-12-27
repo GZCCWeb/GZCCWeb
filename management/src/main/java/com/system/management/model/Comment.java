d0469b12930bec4e453f4df25c1fd0a01655874d
Richard <1265672126@qq.com>
2018-12-27 19:06:27 +0800

    tenant

--- management/src/main/java/com/system/management/model/Comment.java
+++ management/src/main/java/com/system/management/model/Comment.java
@@ -1,9 +1,15 @@
 package com.system.management.model;
 
+import com.mysql.cj.x.protobuf.MysqlxResultset;
+
+import java.sql.Date;
+import java.sql.Time;
+
 public class Comment {
     private int id;
     private String username;
     private String text;
+    private Date comment_time;
 
     public int getId() {
         return id;
@@ -27,5 +33,13 @@ public class Comment {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public Date getComment_time() {
+        return comment_time;
+    }
+
+    public void setComment_time(Date comment_time) {
+        this.comment_time = comment_time;
     }
 }
